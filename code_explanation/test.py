6fbb4298a3fe9b08f1cdc78009a6d93f11ddbf48
TQuynh109 <thaoquynh0109@gmail.com>
2025-08-19 16:09:32 +0700

    test conflict

--- code_explanation/test.py
+++ code_explanation/test.py
@@ -1,11 +1,8 @@
 from transformers import AutoTokenizer
 from huggingface_hub import HfFolder
 
-# huggingface-cli login
-tokenizer = AutoTokenizer.from_pretrained(
-    "meta-llama/Meta-Llama-3-8B-Instruct",
-    token=HfFolder.get_token()
-)
+import os
+import sys
 
 # Prompt ví dụ
 prompt = "<|start_header_id|>user<|end_header_id|>\nWhat is the capital of France?\n<|start_header_id|>assistant<|end_header_id|>\n"
